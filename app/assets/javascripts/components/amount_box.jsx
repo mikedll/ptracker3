3aef7731b8226a293b79e0ff68aa176956b5e966
Michael Rivera <mrivera@michaelriveraco.com>
2018-03-29 00:22:18 -0700

    displays of credits, debits, and balance.

---
+++ app/assets/javascripts/components/amount_box.jsx
@@ -0,0 +1,13 @@
+
+class AmountBox extends React.Component {
+  render() {
+    return(
+      <div className="col-md-4">
+        <div className={'card mb-3 bg-' + this.props.type}>
+          <div className="card-header">{this.props.text}</div>
+          <div className="card-body">{amountFormat(this.props.amount)}</div>
+        </div>
+      </div>
+    );
+  }
+}
--- app/assets/javascripts/components/line_items.jsx
+++ app/assets/javascripts/components/line_items.jsx
@@ -1,21 +1,42 @@
-var LineItems = createReactClass({
-  addLineItem: function(line_item) {
+class LineItems extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      line_items: this.props.data
+    };
+
+    this.addLineItem = this.addLineItem.bind(this);
+  }
+  
+  addLineItem(line_item) {
     var line_items = this.state.line_items;
     line_items.push(line_item);
     this.setState({line_items: line_items});
-  },
-  getInitialState: function() {
-    return {
-      line_items: this.props.data
-    };
-  },
-  getDefaultProps: function() {
-    return { line_items: [] };
-  },
-  render: function() {
+  }
+  
+  credits() {
+    var credits = this.state.line_items.filter(function(val) { return val.amount > 0; });
+    return credits.reduce(function(prev, cur) { return prev + parseFloat(cur.amount); }, 0);
+  }
+
+  debits() {
+    var credits = this.state.line_items.filter(function(val) { return val.amount < 0; });
+    return credits.reduce(function(prev, cur) { return prev + parseFloat(cur.amount); }, 0);
+  }
+
+  balance() {
+    return this.debits() + this.credits();
+  }
+
+  render() {
     return (
       <div className="line_items">
         <h2 className="title"> Line Items </h2>
+        <div className="row">
+          {React.createElement(AmountBox, {type: 'success', amount: this.credits(), text: "Credit"})}
+          {React.createElement(AmountBox, {type: 'danger', amount: this.debits(), text: "Debit"})}
+          {React.createElement(AmountBox, {type: 'info', amount: this.balance(), text: "Balance"})}
+        </div>
         {React.createElement(LineItemForm, {handleNewRecord: this.addLineItem})}
         <hr/>
         <table className="table table-bordered">
@@ -31,4 +52,8 @@ var LineItems = createReactClass({
       </div>
     );
   }
-});
+}
+
+LineItems.defaultProps = {
+  line_items: []
+};
