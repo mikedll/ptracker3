6ee00b58f32b04de48e19953e93a737b04a6cb9a
Michael Rivera <soymrmike@gmail.com>
2018-04-28 03:22:37 -0700

    moved top menu into jsx; items path is not working

--- app/javascript/components/app_root.jsx
+++ app/javascript/components/app_root.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 
 import PurchaseOrders from './purchase_orders';
@@ -11,9 +12,31 @@ import PurchaseOrder from './purchase_order';
 class AppRoot extends React.Component {
 
   render() {
+    const MenuLink = ({ label, to, activeOnlyWhenExact}) => (
+      <Route path={to} exact={activeOnlyWhenExact} children={({ match}) => (
+        <li className={'nav-item' + (match ? ' active' : '')}>
+          <Link className="nav-link" to={to}>{label}</Link>
+        </li>
+      )}/>
+    );
+
     return (
       <Router>
         <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Route path={AppRoutes.root} exact children={() => (
+              <Link className="navbar-brand" to={AppRoutes.root}>Home</Link>
+            )}/>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <MenuLink to={AppRoutes.purchaseOrders} label='Purchase Orders'/>
+                <MenuLink to={AppRoutes.items} label='Items'/>
+              </ul>
+            </div>
+          </nav>
           <Route exact path="/" component={PurchaseOrders}/>
           <Route path="/welcome" component={PurchaseOrders}/>
           <Route exact path="/purchase_orders" component={PurchaseOrders}/>
