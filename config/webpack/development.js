3f8d2ef2cecdcfcc41816f5f1003c9932ef6ae4f
Michael Rivera <soymrmike@gmail.com>
2018-05-07 21:45:52 -0700

    CleanWebpackPlugin introduced to clean public/packs on webpack compiles

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,15 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const environment = require('./environment')
+const path = require('path');
+const environment = require('./environment');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = environment.toWebpackConfig()
+// Disable source maps, in case you want to debug raw webpack output
+// in the browser.
+// environment.config.devtool = null;
+
+environment.plugins.prepend('CleanWebpackPlugin', new CleanWebpackPlugin(['public/packs'], {root: path.resolve(__dirname, '../../')}));
+  
+// console.log(environment.toWebpackConfig());
+
+module.exports = environment.toWebpackConfig();
