1c0f11a4caf57ea3f20b55f0428971c887526b0d
Michael Rivera <soymrmike@gmail.com>
2018-05-08 19:12:53 -0700

    finally devised a workaround for webpack/issues/7248.
    https://github.com/webpack/webpack/issues/7248

--- app/javascript/packs/server_rendering.js
+++ app/javascript/packs/server_rendering.js
@@ -3,3 +3,10 @@
 var componentRequireContext = require.context("components", true)
 var ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(componentRequireContext)
+
+if(typeof(__webpack_require__.e) === "undefined") {
+  __webpack_require__.e = function(unusedChunkId) {
+    return new Promise(function(resolve) { resolve(); });
+  };
+}
+
