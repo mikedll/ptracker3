a6ff3c853483aadc67a647721d965d749fcfb2eb
Michael Rivera <soymrmike@gmail.com>
2018-05-29 20:08:39 -0700

    max on item autocomplete for jqueryUI. removed welcome_controller.
    Replaced welcome#index with purchase_orders#index for root route.

--- app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    @query_result = PurchaseOrder.search(params[:page])
-    render 'shared/app_root'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
       get :autocomplete
     end
   end
-  get 'welcome/index'
 
-  root 'welcome#index'
+  root 'purchase_orders#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
